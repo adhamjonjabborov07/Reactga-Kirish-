b225825b02ecc4f59f3117ff3ad5082846c28b2c
adhamjon0770 <isagi3572@gmail.com>
2025-05-28 18:30:50 +0500

    bir nechta element qoshildi

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,13 @@ function App() {
     <>
     <h1>Welcome to React 🔥</h1>
     <h2>This is my first React app</h2>
+    <ul>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+      <li>React</li>
+    </ul>
+    <h3>What is React?</h3>
     </>
   )
 }
