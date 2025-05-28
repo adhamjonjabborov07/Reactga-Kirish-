4bad7b1aa1c36db45642bbff86d84da0b9e0f4e5
adhamjon0770 <isagi3572@gmail.com>
2025-05-28 19:07:34 +0500

    bir nechta narsalar qoshildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
   return (
     <>
-    <h1>Welcome to React 🔥</h1>
-    <h2>This is my first React app</h2>
-    <ul>
-      <li>HTML</li>
-      <li>CSS</li>
-      <li>JavaScript</li>
-      <li>React</li>
-    </ul>
-    <h3>What is React?</h3>
+      <div className="ELEMENTS-DIV">
+        <h1>Welcome to React 🔥</h1>
+        <h2>This is my first React app</h2>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>React</li>
+        </ul>
+        <h2>What is React?</h2>
+        <p>
+          React — bu JavaScript kutubxonasi (library) bo‘lib, foydalanuvchi
+          interfeyslarini (ya’ni veb-ilovalar oynasini) yaratish uchun
+          ishlatiladi. Uni Facebook (hozirgi Meta) ishlab chiqqan va ochiq
+          manbali (open-source) hisoblanadi.
+        </p>
+        <h2>What is Sungle-page aplication (SPA) </h2>
+        <p>
+          Single-Page Application (SPA) — bu bitta sahifali veb-ilova degani.
+          Ya’ni, ilova birgina HTML sahifadan iborat bo‘ladi va foydalanuvchi
+          ilova ichida harakat qilganda (masalan, boshqa bo‘limga o‘tganda)
+          butun sahifa qayta yuklanmaydi, faqat kerakli qismlar dinamik tarzda
+          yangilanadi.
+        </p>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
